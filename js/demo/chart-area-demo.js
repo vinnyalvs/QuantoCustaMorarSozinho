ce0aa6c1dd35d3c5dcb7626d4f97e42b1e62ec6e
vinnyalvs <viniciusalves@ice.ufjf.br>
2020-08-16 21:15:00 -0300

    atualizando página inicial com campos e removendo codigo desnecessario

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -28,7 +28,7 @@ function number_format(number, decimals, dec_point, thousands_sep) {
 }
 
 // Area Chart Example
-var ctx = document.getElementById("myAreaChart");
+/* var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
@@ -115,4 +115,4 @@ var myLineChart = new Chart(ctx, {
       }
     }
   }
-});
+}); */
